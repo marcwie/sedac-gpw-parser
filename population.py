c2f64ba3813637d873945b7cf5e3f3c405c5bb48
Marc Wiedermann <marcwiedermann@posteo.de>
2020-03-13 15:32:40 +0100

    styleguide compliance

--- population.py
+++ population.py
@@ -1,11 +1,40 @@
 import os
 import numpy as np
-from matplotlib import pyplot as plt
-from matplotlib import cm
 from grid import Grid
 
 POPULATION_FILE_NAME = "gpw_v4_population_count_rev11_2020_30_sec_{0}.asc"
 POP_OUTPUT_FILE_NAME = "{0}_population.txt"
+
+
+def _decompress(indices):
+    decompressed = []
+    for entry in indices.split(" "):
+        if "x" in entry:
+            counter, value = entry.split("x")
+        else:
+            counter = 1
+            value = entry
+        decompressed.extend(int(counter) * [float(value)])
+    return decompressed
+
+
+def _compress(array):
+
+    indices = ""
+
+    start = array[0]
+    counter = 1
+    for _ in range(1, len(array)):
+        if array[_] == start:
+            counter += 1
+        else:
+            indices += str(counter) + "x"+str(start) + " "
+            start = array[_]
+            counter = 1
+    indices += str(counter) + "x"+str(start)
+
+    return indices
+
 
 class Population(Grid):
 
@@ -16,7 +45,7 @@ class Population(Grid):
                  overwrite=False):
 
         assert not overwrite, "Not implemented yet!"
-      
+
         pop_output_file_name = POP_OUTPUT_FILE_NAME.format(country_id)
 
         self._country_id = country_id
@@ -35,43 +64,33 @@ class Population(Grid):
             self.save_compressed_population()
         print("Loading population...")
         self.load_compressed_population()
-        
+
         print("Total population:", self.total_population())
-             
-    
+
+
     def total_population(self):
         population = self._population
-        total_population = population[population>0].sum()
+        total_population = population[population > 0].sum()
 
         return total_population
 
 
-    def decompress(self, indices):
-        decompressed = []
-        for entry in indices.split(" "):
-            if "x" in entry:
-                counter, value = entry.split("x")
-            else: 
-                counter = 1
-                value = entry
-            decompressed.extend(int(counter) * [float(value)])
-        return decompressed 
 
 
     def load_compressed_population(self):
 
-        input_file = self._population_output_path 
+        input_file = self._population_output_path
 
         population = []
-        
+
         with open(input_file, "r") as infile:
-            
-            n_col = int(infile.readline()[:-1].split(" ")[-1]) 
-            n_row = int(infile.readline()[:-1].split(" ")[-1]) 
-            self._llcrnrlon = float(infile.readline()[:-1].split(" ")[-1]) 
-            self._llcrnrlat = float(infile.readline()[:-1].split(" ")[-1]) 
-            self._cellsize = float(infile.readline()[:-1].split(" ")[-1]) 
-            
+
+            n_col = int(infile.readline()[:-1].split(" ")[-1])
+            n_row = int(infile.readline()[:-1].split(" ")[-1])
+            self._llcrnrlon = float(infile.readline()[:-1].split(" ")[-1])
+            self._llcrnrlat = float(infile.readline()[:-1].split(" ")[-1])
+            self._cellsize = float(infile.readline()[:-1].split(" ")[-1])
+
             for _ in range(2):
                 infile.readline()
 
@@ -79,39 +98,23 @@ class Population(Grid):
 
             for _ in range(n_row):
                 indices = infile.readline()
-                decompressed = self.decompress(indices) 
+                decompressed = _decompress(indices)
                 population[_] = np.array(decompressed)
                 print(_, end="\r")
-                
+
         self._population = population
         print("Done..")
 
 
-    def _compress(self, array):
-    
-        indices = ""
-
-        start = array[0]
-        counter = 1
-        for _ in range(1, len(array)):
-            if array[_] == start:
-                counter += 1
-            else:
-                indices += str(counter) + "x"+str(start) + " "
-                start = array[_]
-                counter = 1
-        indices += str(counter) + "x"+str(start)
-
-        return indices
 
 
     def save_compressed_population(self):
-    
-        output_filepath = self._population_output_path 
+
+        output_filepath = self._population_output_path
         population = self._population
-        
+
         max_value = len(population)
-        
+
         n_row, n_col = population.shape
         outstring = ""
         outstring += "ncols {0}\n".format(n_col)
@@ -124,72 +127,70 @@ class Population(Grid):
         print(outstring)
         for _, entry in enumerate(population):
             print(_, max_value, end="\r")
-            outstring += self._compress(entry)+"\n"
+            outstring += _compress(entry)+"\n"
 
         with open(output_filepath, "w") as outfile:
             outfile.write(outstring)
 
 
     def parse_population(self, accuracy=3):
-        
+
         print("Parsing population...")
         coords = self._country_coords
         input_path = self._input_path
 
-        population = np.zeros((10800*2, 10800*4)) 
-    
+        population = np.zeros((10800*2, 10800*4))
+
         valid_x = set()
         valid_y = set()
-       
+
         for file_id, file_coords in coords.items():
-    
+
             x_offset = 10800 * ((file_id-1) % 4)
             y_offset = 10800 * (file_id > 4)
-            
+
             with open(input_path.format(file_id)) as infile:
-    
+
                 for _ in range(4):
                     infile.readline()
-                
+
                 cellsize = infile.readline()
                 cellsize = float(cellsize[:-1].split(" ")[-1])
-                
+
                 infile.readline()
 
                 all_y = list(file_coords.keys())
                 min_index = np.min(all_y)
                 max_index = np.max(all_y)
-                
+
                 for _ in range(min_index):
                     infile.readline()
-                
+
                 for i in range(max_index - min_index + 1):
                     print(file_id, i, len(population), end="\r")
-                    y = min_index + i
-                    if y not in all_y:
+                    row_id = min_index + i
+                    if row_id not in all_y:
                         continue
-    
-                    x = file_coords[y]
-    
+
+                    col_id = file_coords[row_id]
+
                     line = infile.readline()
                     line = line.split(" ")
                     assert line[0] != ""
                     assert line[-1] == "\n"
                     assert len(line) == 10801
-    
-                    coords_x = [_x + x_offset for _x in x]
-                    coords_y = [ y + y_offset ] * len(x)
-    
-                    #coords_y = [ 10800 - y - 1 + y_offset ] * len(x)
+
+                    coords_x = [_x + x_offset for _x in col_id]
+                    coords_y = [row_id + y_offset] * len(col_id)
+
                     valid_x.update(coords_x)
-                    valid_y.update((y + y_offset,))
-    
-                    pop = [float(line[_x]) + 2 for _x in x]
+                    valid_y.update((row_id + y_offset,))
+
+                    pop = [float(line[_x]) + 2 for _x in col_id]
                     population[(coords_y, coords_x)] = pop
 
                 print()
-                #data[i] = np.array(line.split(" ")).astype(float)
-       
+
         min_x = np.min(list(valid_x))
         max_x = np.max(list(valid_x))
         min_y = np.min(list(valid_y))
@@ -198,9 +199,11 @@ class Population(Grid):
         population = population[min_y:(max_y+1), min_x:(max_x+1)]
         population -= 2
         population[population < -1000] = -1
-     
+
         max_value = len(population)
-        for i in range(len(population)):
+
+        # Do this on for-loop to be light on memory
+        for i in range(max_value):
             print("Rounding:", i, max_value, end="\r")
             population[i] = np.round(population[i], accuracy)
 
@@ -208,8 +211,11 @@ class Population(Grid):
         self._population = population
         self._llcrnrlon = (min_x * cellsize) % 360 - 180
         self._llcrnrlat = (180 - max_y * cellsize) % 180 - 90
-        self._cellsize = cellsize 
-        
+        self._cellsize = cellsize
+
+
+def main():
+    Population(country_id=68)
 
 if __name__ == "__main__":
-    pop = Population(country_id=68)
+    main()

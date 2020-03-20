552d4e14a2f09c8870815c6cac6f981b992f0ef4
Marc Wiedermann <marcwiedermann@posteo.de>
2020-03-20 12:50:04 +0100

    short docstring with reference to README.md

--- sedac_gpw_parser/__init__.py
+++ sedac_gpw_parser/__init__.py
@@ -1,3 +1,11 @@
+"""
+sedac-gpw-parser provides a framework to parse and work with high resolution
+gridded populaton estimates from the Socioeconomic Data and Applications
+Center.
+
+For details on the installation and usage see the package's README.md available
+at https://github.com/marcwie/sedac-gpw-parser.
+"""
 from . import grid
 from . import population
 from . import plot

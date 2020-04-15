b5b9ce1366c8c35dc3e0615cc531ccafa1727099
Marc Wiedermann <marcwiedermann@posteo.de>
2020-04-15 15:56:54 +0200

    add switch for verbosity of output

--- sedac_gpw_parser/utils.py
+++ sedac_gpw_parser/utils.py
@@ -4,7 +4,8 @@ GRID_FOLDER = "gpw-v4-national-identifier-grid-rev11_30_sec_asc/"
 GRID_LOOKUP = "gpw_v4_national_identifier_grid_rev11_lookup.txt"
 DATA_FOLDER = os.path.expanduser("~") + "/.sedac_gpw_parser/"
 
-def id_lookup(searchterm, lookup_file=DATA_FOLDER+GRID_FOLDER+GRID_LOOKUP):
+def id_lookup(searchterm, lookup_file=DATA_FOLDER+GRID_FOLDER+GRID_LOOKUP,
+              verbose=True):
 
     success = False
 
@@ -21,11 +22,13 @@ def id_lookup(searchterm, lookup_file=DATA_FOLDER+GRID_FOLDER+GRID_LOOKUP):
             country_name = line[3]
             
             if searchterm.lower() in country_name.lower().replace(" ", ""):
-                print(country_name, ":", country_id)
+                if verbose:
+                    print(country_name, ":", country_id)
                 success = True
                 names_ids.append((country_name, int(country_id)))
 
     if not success:
-        print("No country found for search term:", searchterm)
+        if verbose:
+            print("No country found for search term:", searchterm)
 
     return names_ids
